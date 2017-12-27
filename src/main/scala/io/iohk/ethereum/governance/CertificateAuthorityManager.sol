f30e334097f65045793e4ea330f0f0279c6ef8b9
github_iohk.com <mirko.alic@iohk.io>
2017-12-27 09:28:40 -0300

    [PO-80] Add addCA/removeCA methods

--- src/main/scala/io/iohk/ethereum/governance/CertificateAuthorityManager.sol
+++ src/main/scala/io/iohk/ethereum/governance/CertificateAuthorityManager.sol
@@ -2,13 +2,67 @@ pragma solidity ^0.4.11;
 
 contract CertificateAuthorityManager {
 
-	event CAQueriedFor(address sender, uint slotNumber);
+	address[] internal certificateAuthorities =  [0x00b15396fbf95ced0bc4a961239ff6350cca2b3203];
 
-	address a1 = 0x00edde8656c35fcb7126c61fc6e2673734425a72bf;
-
-	function isCertificateAuthorityFor(address sender) constant returns(bool) {
-		CAQueriedFor(sender, 0);
-		// TODO: Implement.
-		return a1 == sender;
+	function getCertificateAuthorities() public constant returns(address[]) {
+		return certificateAuthorities;
 	}
+
+	function isCertificateAuthorityFor(address sender) public constant returns(bool) {
+		bool isCA = false;
+		for (uint it = 0; it < certificateAuthorities.length; it++) {
+			if (certificateAuthorities[it] == sender) {
+				isCA = true;
+			}
+    	}
+		return isCA;
+	}
+
+	function addCA(address newCA) public returns(address[]) {
+		bool hasAuthority = false;
+		bool isNew = true;
+		for (uint it = 0; it < certificateAuthorities.length; it++) {
+    	    if (certificateAuthorities[it] == msg.sender) {
+    	       	hasAuthority = true;
+    	    }
+			if (certificateAuthorities[it] == newCA) {
+				isNew = false;
+			}
+    	}
+		if (hasAuthority && isNew) {
+			certificateAuthorities.push(newCA);
+		}
+		return certificateAuthorities;
+	}
+	
+	function removeCA(address ca) public returns(address[]) {
+		bool hasAuthority = false;
+		bool isCA = false;
+		uint removeIndex = 0;
+		for (uint it = 0; it < certificateAuthorities.length; it++) {
+    	    if (certificateAuthorities[it] == msg.sender) {
+    	       	hasAuthority = true;
+    	    }
+			if (certificateAuthorities[it] == ca) {
+				isCA = true;
+				removeIndex = it;
+			}
+    	}
+		if (hasAuthority && isCA) {
+			certificateAuthorities = remove(certificateAuthorities, removeIndex);
+		}
+		return certificateAuthorities;
+	}
+
+	function remove(address[] accounts, uint removeIndex) internal pure returns(address[]) {	
+        if (removeIndex >= accounts.length) return;
+		address[] memory arrayNew = new address[](accounts.length - 1);
+        for (uint itBefore = 0; itBefore < removeIndex; itBefore++) {
+            arrayNew[itBefore] = accounts[itBefore];
+        }
+		for (uint itAfter = removeIndex; itAfter < arrayNew.length; itAfter++) {
+            arrayNew[itAfter] = accounts[itAfter];
+        }
+        return arrayNew;
+    }
 }
